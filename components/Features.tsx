21ca88f4adb8917e8456464f91b41e39b40a05a8
Deepupradhan <118422944+Deepupradhan@users.noreply.github.com>
2024-04-18 16:45:42 +0530

    color change

--- components/Features.tsx
+++ components/Features.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Features = () => {
   return (
     <section className="flex-col  flexCenter  overflow-hidden bg-feature-bg bg-center bg-no-repeat  py-40  ">
-      <div className="max-container padding-container relative w-full flex justify-end">
+      <div className="max-container bg-gray-100 padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
@@ -52,14 +52,14 @@ type FeatureItem = {
 
 const FeatureItem = ({ title, icon, description }: FeatureItem) => {
   return (
-    <li className="flex w-full flex-1 flex-col items-start">
-      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+    <li className="flex w-full flex-1 flex-col items-start bg-gray-100">
+      <div className="rounded-full p-4 lg:p-7 bg-orange-50">
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
       <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
         {title}
       </h2>
-      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+      <p className="regular-16 mt-5 bg-gray-100 text-gray-30 lg:mt-[30px] lg:bg-none">
         {description}
       </p>
     </li>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className='flex flexBetween shadow-md  padding-container  relative z-30 py-5'>
         <Link href='/'>
             {/* <Image src='/hilink-logo.svg' alt='logo' width={74} height={29}/> */}
-            <h1 className=' font-extrabold text-2xl' ><span className='text-green-50'>E</span>xplore<span className='text-green-50'>E</span>pic</h1>
+            <h1 className=' font-extrabold text-2xl' ><span className='text-orange-50'>E</span>xplore<span className='text-orange-50'>E</span>pic</h1>
         </Link>
         <ul className='hidden h-full gap-12 lg:flex'>
             {NAV_LINKS.map((link)=>(
